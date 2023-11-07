97c3793063d682ac3a9830393b2d394b0ea6049a
alondra zurita <alondrazurita2@gmail.com>
2023-11-06 23:22:27 -0600

    fixing promblems

--- utils/data.js
+++ utils/data.js
@@ -1,27 +1,69 @@
 const names = [
-    'Alex',
-    'John',
-    'David',
-    'Sarah',
-    'Greg',
-    'Logan',
-    'Maria',
-    'Julia',
-    'Shirley',
-    'Daisy',
-    'Benito',
-    'Loki',
-    'Athena',
-    'Taylor',
-    'Maty',
-    'Joe',
-    'Corbin',
-    'Matt',
-    'Jordan',
-    'Stephanie'
+    'Alex Russo',
+    'John Brown',
+    'David Johnson',
+    'Sarah Knott',
+    'Greg Floyd',
+    'Logan Miles',
+    'Maria Lopez',
+    'Julia Goodwin',
+    'Shirley Frazier',
+    'Daisy Fields',
+    'Benito Ramirez',
+    'Loki Zurita',
+    'Athena Ramirez',
+    'Taylor Swift',
+    'Maty Reyes',
+    'Joe Snugg',
+    'Corbin Taylor',
+    'Matt Hahnman',
+    'Jordan Miles',
+    'Stephanie Green'
 ];
+const comments = [
+    'Decision Trackers are awesome',
+    'Find My Phone is a useful app',
+    'Learn Piano is not very good for learning Piano',
+    'Starbase Defender is a great game, I love it',
+    'Tower Defense is okay',
+    'Monopoly Money is better than real money IMO',
+    'Movie trailers are just the best parts of a movie distilled into 90 seconds',
+    'Hello world, this is a comment',
+    'Social media is a big waste of time',
+    'Notes is my most used app',
+    'Messages is open on my computer 24/7',
+    'Email is open on my computer',
+    'Compass is never opened',
+    'Firefox is great for privacy',
+  ];
+  
 const users = [];
 const getNamePart = () => names[Math.floor(Math.random() * names.length)];
-const getRandomName = () => `${getNamePart()} ${getNamePart()}`;
+const getRandomName = () => 
+`${getNamePart()} ${getNamePart()}`;
 
-module.exports = getRandomName;
+const getRandomPosts = (int) => {
+    const results = [];
+    for (let i = 0; i < int; i++) {
+      results.push({
+        username: getRandomName().split(' ')[0],
+      });
+    }
+    return results;
+  };
+  const getRandomComments = (int) => {
+    const results = [];
+    for (let i = 0; i < int; i++) {
+      results.push({
+        username: getRandomName().split(' ')[0],
+      });
+    }
+    return results;
+  };
+  
+  
+module.exports = {
+    getRandomName,
+    getRandomPosts,
+    getRandomComments,
+};
--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,11 @@
 const connection = require('../config/connection');
-const { Post, User } = require('../models');
-const getRandomName = require('./data');
+const { Post, User, Comment } = require('../models');
+const {
+  getRandomName,
+  getRandomComments,
+  getRandomPost,
+  genRandomIndex,
+} = require('./data');
 
 console.log(getRandomName());
 connection.on('error', (err) => err);
@@ -32,6 +37,29 @@ connection.once('open', async () => {
   }
 
   await User.collection.insertMany(users);
+
+  // Empty arrays for randomly generated posts and comments
+  const comments = [...getRandomComments(10)];
+  const posts = [];
+
+  // Makes comments array
+  const makePost = (text) => {
+    posts.push({
+      text,
+      username: getRandomName().split(' ')[0],
+      comments: [comments[genRandomIndex(comments)]._id],
+    });
+  };
+
+  // Wait for the comments to be inserted into the database
+  await Comment.collection.insertMany(comments);
+
+  // For each of the comments that exist, make a random post of 10 words
+  comments.forEach(() => makePost(getRandomPost(10)));
+
+  // Wait for the posts array to be inserted into the database
+  await Post.collection.insertMany(posts);
+
   console.log(users);
   console.timeEnd('seeding complete 🌱');
   process.exit(0);
